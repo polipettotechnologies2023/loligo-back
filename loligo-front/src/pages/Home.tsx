d34958bdbc1f394b88ff6d55637e937e8515db55
Wesley <obi.wesley.giovanni@gmail.com>
2024-01-20 11:50:16 +0100

    test deploy on vercel

--- loligo-front/src/pages/Home.tsx
+++ loligo-front/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import RequestsLists from "../components/RequestsLIsts"
 
 
 export default function Home() {
 
-  const { user } = useAuth0();
+  // const { user } = useAuth0();
 
 
     return (
