3e615360696cb9c61093144fbe105b478f1d3884
Alexis Salas <alexissalas@MacBook-Pro-de-Alexis.local>
2024-01-25 15:28:15 +0100

    Button now redirects to jira

--- loligo-front/src/components/LoginButonExpert.tsx
+++ loligo-front/src/components/LoginButonExpert.tsx
@@ -2,9 +2,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "@nextui-org/react";
 
 const LoginButtonExpert = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <Button radius="full" size="lg" variant="bordered" color="secondary" onClick={() => loginWithRedirect()}>Login for experts</Button>;
+  const jira_board = 'https://polipetto-lolligo.atlassian.net/jira/software/projects/LOL/boards/1?atlOrigin=eyJpIjoiZTU4MTA2YzkxZTAyNGY3M2I3MDdkMDA0MDIyNDI5M2EiLCJwIjoiaiJ9'
+  return <Button radius="full" size="lg" variant="bordered" color="secondary" onClick={ () => { window.location.href = jira_board; }}>Login for experts</Button>;
 };
 
 export default LoginButtonExpert;
