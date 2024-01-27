37141c746e6cba31d5b8c4fd3b73615f6da46ecc
Wesley <obi.wesley.giovanni@gmail.com>
2024-01-27 09:18:54 +0100

    nav redesign

--- loligo-front/src/components/CustomNav.tsx
+++ loligo-front/src/components/CustomNav.tsx
@@ -1,47 +1,40 @@
-import CustomAvatar from "./customAvatar"
-// import import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Checkbox, Input, Link} from "@nextui-org/react";
+import CustomAvatar from "./customAvatar";
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
 
 export default function CustomNav() {
+
   return (
-  <>
-  <div className="container"style={{
-        marginTop:"2em",
-        display: "flex",
-        maxHeight: "6em",
-        maxWidth:"100%"
-      }}>
-        <div className="column" style={{
-          margin:"1em 1em 0 0",
-          flex: "1",
-          display:"flex",
-          alignItems:"center",
-          justifyContent: "center",
-        }}>
-          <img src="loligo_branding_logo.svg" style={{
-            scale:".1"
-          }}></img>
-        </div>
-        <div className="column" style={{
-          margin:"1em 1em 0 0",
-          flex: "1",
-          display:"flex",
-          alignItems:"center",
-          justifyContent: "center",
-        }}>
-          <img src="loligo_textonly_logo.svg" style={{
-            scale: ".3"}
-          }></img>
-        </div>
-        <div className="column" style={{
-          margin:"1em 1em 0 0",
-          flex: "1",
-          display:"flex",
-          alignItems:"center",
-          justifyContent: "center",
-        }}>
-          <CustomAvatar></CustomAvatar>
-        </div>
-      </div>
-  </>
-    )
-      }
+    <>
+      <Navbar
+        style={{
+          marginTop: "2em",
+          display: "flex",
+          maxHeight: "6em",
+          maxWidth: "100%",
+        }}
+      >
+        <NavbarContent>
+          <NavbarBrand>
+            <img src="loligo_branding_logo.svg" width="25%"></img>
+          </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent>
+        <NavbarBrand>
+          <img src="loligo_textonly_logo.svg" width="100%"></img>
+        </NavbarBrand>
+        </NavbarContent>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <CustomAvatar></CustomAvatar>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </>
+  );
+}
