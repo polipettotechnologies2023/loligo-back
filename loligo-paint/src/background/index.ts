7310b59eda2bdf0a63b1433471171a37be4e3d6f
Wesley <obi.wesley.giovanni@gmail.com>
2024-02-05 09:24:40 +0100

    added open on commads

--- loligo-paint/src/background/index.ts
+++ loligo-paint/src/background/index.ts
@@ -1,13 +1,10 @@
-// export {}
-// import type { PlasmoCSConfig } from "plasmo"
-// import { Storage } from "@plasmohq/storage"
- 
+export {}
+import { sendToBackground } from "@plasmohq/messaging"
+import { windowCanvas } from "~contents/window-canvas"
 // export const config: PlasmoCSConfig = {
 //   matches: ["https://polipetto-lolligo.atlassian.net/jira/software/projects/LOL/boards/1?selectedIssue=*"],
 //   all_frames: true
 // }
-
-// const storage = new Storage()
 
 // chrome.tabs.onUpdated.addListener(function
 //    (tabId, changeInfo, tab) {
@@ -21,11 +18,68 @@
 //    }
 //  );
 
+// TODO: find a way to call the plug in by commands
+chrome.commands.onCommand.addListener((command, tab) => {
+    if(command == "Screenshot"){
+    chrome.tabs.captureVisibleTab(null, {}, (base64) => {
+    chrome.scripting.executeScript({
+      args: [base64],
+      target: { tabId: tab.id },
+      func: async (args) => {
+        try {
+          console.log("injected")
+          console.log(args)
+          navigator.clipboard.writeText(args)
+          const file1Url = chrome.runtime.getURL("tabs/AppCanvas.html")
+
+          chrome.runtime.sendMessage({
+            action: "createWindow",
+            argument: file1Url
+          }, function(response) {
+            console.log("Response from background script:", response);
+          });
+          
 
 
-//TODO: find a way to call the plug in by commands
-// chrome.commands.onCommand.addListener((command) => {
+        } catch (error) {
+          console.error("Error capturing screenshot:", error)
+        }
+      }
+    })
+  })
+}else if (command == "WhiteCanvas") {
    
+        chrome.scripting.executeScript({
+          args: ['hello'],
+          target: { tabId: tab.id },
+          func: async (args) => {
+            try {''
+              console.log("injected")
+              console.log(args)
+              navigator.clipboard.writeText("nulla")
+              const file1Url = chrome.runtime.getURL("tabs/AppCanvas.html")
+    
+              chrome.runtime.sendMessage({
+                action: "createWindow",
+                argument: file1Url
+              }, function(response) {
+                console.log("Response from background script:", response);
+              });
+              
+    
+    
+            } catch (error) {
+              console.error("Error capturing screenshot:", error)
+            }
+          }
+        })
 
-//  });
+}
+})
 
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action === "createWindow") {
+        chrome.windows.create(
+            { url: message.argument, type: "popup", width: 1920, height: 1080 },)
+    }
+  });
--- loligo-paint/src/popup.tsx
+++ loligo-paint/src/popup.tsx
@@ -2,17 +2,14 @@ import { useEffect } from "react"
 import { windowCanvas } from "./contents/window-canvas"
 
 function IndexPopup() {
-  useEffect(()=>{
-    (() => {
-      chrome.tabs.captureVisibleTab(null, {}, function (image) {
-        navigator.clipboard.writeText(image)
-        windowCanvas(image)
-     });
-    })();
-  },[])
     
   return (
     <>
+    <div>
+    <h3>Use the one of the following commands:</h3>
+    <p>Screenshot current tab:<b> CTRL + Shift + L</b> | <b>Command + Shift + L</b> </p>
+    <p>White Canvas: <b>CTRL + Shift + W</b> | <b>Command + Shift + W</b> </p>
+    </div>
     </>
   )
 }
