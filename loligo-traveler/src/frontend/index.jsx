2d6974b1301231de47c39c3ab2c7ec20ee60e55f
Wesley <obi.wesley.giovanni@gmail.com>
2024-02-06 11:05:04 +0100

    remove loligo traveler

--- loligo-traveler/src/frontend/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ForgeReconciler from "@forge/react";
-import { App } from "./App";
-
-ForgeReconciler.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
