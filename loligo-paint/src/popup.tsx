f785eefe6e64f9269dfda09c8219544d53896e23
wyo <wesley.obi@communardo.com>
2024-02-06 11:30:26 +0100

    change of commands on win

--- loligo-paint/src/popup.tsx
+++ loligo-paint/src/popup.tsx
@@ -4,8 +4,8 @@ export default function IndexPopup() {
     <>
     <div>
     <h3>Use the one of the following commands:</h3>
-    <p>Screenshot current tab:<b> CTRL + Shift + P</b> | <b>Command + Shift + P</b> </p>
-    <p>White Canvas: <b>CTRL + Shift + O</b> | <b>Command + Shift + O</b> </p>
+    <p>Screenshot current tab:<b> CTRL + Shift + L</b> | <b>Command + Shift + P</b> </p>
+    <p>White Canvas: <b>CTRL + Shift + E</b> | <b>Command + Shift + O</b> </p>
     <h3>Please give loligo paint full access to your cliboard. We are not sharing or saving any data. All the communication are happening between you and your jira board</h3>
     </div>
     </>
