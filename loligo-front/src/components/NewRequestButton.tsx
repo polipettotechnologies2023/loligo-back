189fbbad3d489850a17224580f87b8a2dfc597b3
Alexis Salas <alexissalas@e-10-57-0cc6.local.tu-chemnitz.de>
2024-02-26 17:24:18 +0100

    Statuses behavior update

--- loligo-front/src/components/NewRequestButton.tsx
+++ loligo-front/src/components/NewRequestButton.tsx
@@ -10,7 +10,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../vite-env";
-import ConfermationModal from "./ConfermationModal"
+import ConfermationModal from "./ConfermationModal";
+
 
 
 export default function newRequestButton() {
@@ -20,15 +21,17 @@ export default function newRequestButton() {
   const [webSiteLink, setWebsiteLink] = useState("");
   const [confermationModal, setConfermationModal] = useState<any>(undefined);
   const [disabled, setDisabled] = useState<any>(false)
-
+  const [errorMsgName, setErrorMessageName] = useState("");
+  const [errorMsgLink, setErrorMessageLink] = useState("");
   const token = useSelector((state: RootState) => state.token.value);
-
+    
   useEffect(()=>{
     
   },[confermationModal,disabled])
 
   const SubmitButtonHandler = async () => {
-
+    if (webSiteLink !== "https://" && websiteName !== "" && webSiteLink != ""){
+      console.log("WEBSITE LINK VALUE:" + webSiteLink);
     if (user?.sub) {
       let user_id = extractUserId(user?.sub);
       let res = await axios.post(
@@ -48,6 +51,7 @@ export default function newRequestButton() {
       );
 
       if (res.status == 200) {
+        setDisabled(true)
         return setConfermationModal(<ConfermationModal></ConfermationModal>)
       } else {
         //TODO: Change this with a good handler
@@ -55,6 +59,16 @@ export default function newRequestButton() {
         console.log(res);
       }
     }
+  } else if(webSiteLink == "https://" || webSiteLink == "" && websiteName != "") {
+    setErrorMessageName("");
+    setErrorMessageLink("Please provde the website's url");
+  } else if (websiteName == "" && webSiteLink != "https://"){
+    setErrorMessageName("Please provide the website's name");
+    setErrorMessageLink("");
+  } else if (webSiteLink == "https://" || webSiteLink == "" && websiteName == ""){
+    setErrorMessageName("Please provide the website's name");
+    setErrorMessageLink("Please provde the website's url");
+  }
   };
 
   function extractUserId(userString: string) {
@@ -87,7 +101,7 @@ export default function newRequestButton() {
           >
             <div className="text-small font-bold">New request</div>
             <div className="text-tiny">Please provide the following: </div>
-            <p
+            <label
               style={{
                 //fontSize: "1rem",
                 lineHeight: "2",
@@ -95,11 +109,13 @@ export default function newRequestButton() {
               }}
             >
               Your website's name:{" "}
-            </p>
+            </label>
             <Input
               type="websiteName"
               isRequired
+              label="Website Name"
               placeholder="i.e. Loligo"
+              errorMessage={errorMsgName}
               style={{}}
               onChange={(e) => setWebsiteName(e.target.value)}
             />
@@ -109,20 +125,28 @@ export default function newRequestButton() {
               paddingTop: "1em",
             }}
           >
-            <p
+            <label
               style={{
                 fontSize: "1rem",
                 lineHeight: "2",
                 fontWeight: "bold",
               }}
+              
             >
               Your base URL:{" "}
-            </p>
+            </label>
             <Input
-              type="websiteName"
-              isRequired={true}
-              placeholder="i.e. https://www.loligo.com/"
-              onChange={(e) => setWebsiteLink(e.target.value)}
+              type="url"
+              label="Website URL"
+              isRequired
+              placeholder="www.loligo.com"
+              errorMessage={errorMsgLink}
+              onChange={(e) => {setWebsiteLink("https://" + e.target.value); console.log(e.target.value)}}
+              startContent={
+                <div className="pointer-events-none flex items-center">
+                  <span className="text-default-400 text-small">https://</span>
+                </div>
+              }
             />
             <div
               style={{
@@ -136,7 +160,7 @@ export default function newRequestButton() {
               <Button
                 className="bg-gradient-to-tr from-purple-950 via-purple-800 to-fuchsia-500 text-white"
                 isDisabled={disabled}
-                onClick={() => {SubmitButtonHandler(); setDisabled(true)}}
+                onClick={() => {SubmitButtonHandler(); }}
               >
                 Submit
               </Button>
